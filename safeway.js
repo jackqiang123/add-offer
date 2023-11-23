a1cc2b6befda01bea30867acca3e758cb739de1c
J Lu <llatsinghua@gmail.com>
2023-11-22 17:58:02 -0800

    Update safeway.js

--- safeway.js
+++ safeway.js
@@ -3,16 +3,17 @@ function sleep(ms) {
 }
 
 async function click() { 
-var loadMore = document.getElementsByClassName('btn load-more')[0];
-while(typeof loadMore !== 'undefined') { 
-	loadMore.click();
-	loadMore = document.getElementsByClassName('btn load-more')[0];
-	await sleep(500);
+	var loadMore = document.getElementsByClassName('btn load-more')[0];
+	while(typeof loadMore !== 'undefined') { 
+		loadMore.click();
+		loadMore = document.getElementsByClassName('btn load-more')[0];
+		await sleep(500);
+	}
+
+	var buttons = document.getElementsByClassName('btn grid-coupon-btn btn-default');
+	for(i=0; i<buttons.length; i++) { 
+		buttons[i].click(); 
+	}
 }
 
-var buttons = document.getElementsByClassName('btn grid-coupon-btn btn-default');
-for(i=0; i<buttons.length; i++) { 
-	buttons[i].click(); 
-}
-
-}
+click();
